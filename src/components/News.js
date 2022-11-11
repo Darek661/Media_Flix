9ef98b30eb61e8724d18ffffeba533880fa6d4a3
Yash Purohit <45822567+Y1a2s3h4@users.noreply.github.com>
2022-11-11 10:15:00 +0530

    Code

--- src/components/News.js
+++ src/components/News.js
@@ -10,54 +10,34 @@ export default function News() {
   const fetch_News = () => {
     setLoading(true);
     fetch(
-      `https://bing-news-search1.p.rapidapi.com/news?category=entertainment&mkt=en-IN&safeSearch=Off&textFormat=Raw`,
-      {
-        method: "GET",
-        headers: {
-          "X-BingApis-SDK": "true",
-          "X-RapidAPI-Key":
-            "678afc3fabmshc0937d1b8c0b77cp17e8c7jsn2e631e9b2e7c",
-          "X-RapidAPI-Host": "bing-news-search1.p.rapidapi.com",
-        },
-      }
+      `https://newsapi.org/v2/top-headlines?country=in&apiKey=4f0ed34c2436488fbea43a2b2255489e&category=entertainment`
     )
-      .then((res) => res.json())
-      .then((data) => {
+      .then(res => res.json())
+      .then(data => {
         console.log(data);
-        setNData(data.value);
+        setNData(data.articles);
         setLoading(false);
       });
   };
 
-  return (
-    <div className="marginTop-c">
-      <center className="mt-5">
-        <h1 className="white c-title d-inline text-uppercase">
-          Entertainment News
+@@ -24,6 +28,7 @@ export default function News() {
         </h1>
       </center>
       <div className="container">
         {isLoading && <Preloader />}
         <div className="row">
-          {news_data.map((items) => (
+          {news_data.map(items => (
             <div className="col-md-6">
               <div
                 class="card h-75 news__card mb-4"
                 style={{ boxShadow: "0 2px 30px rgba(0, 0, 0, .1)" }}
               >
-                <div class="card-header">{items.name}</div>
+                <div class="card-header">{items.source.name}</div>
                 <div class="card-body">
                   <blockquote class="blockquote mb-0">
-                    <p style={{ fontSize: 16 }}>{items.description}</p>
+                    <p style={{ fontSize: 16 }}>{items.title}</p>
                     <footer class="blockquote-footer">
-                      <cite title="Source Title">
-                        {items.provider[0].name},{" "}
-                        {
-                          new Date(items.datePublished)
-                            .toISOString()
-                            .split("T")[0]
-                        }
-                      </cite>
+                      <cite title="Source Title">{items.author}</cite>
                     </footer>
                   </blockquote>
                 </div>
